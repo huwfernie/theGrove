d36fc670d0723a7450f52e5b7184a327124a471e
Huw Fernie <HuwFernie@gmail.com>
2018-12-29 09:22:54 +0000

    initial commit for Grove starter code

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+const gulp       = require('gulp');
+const babel      = require('gulp-babel');
+const sass       = require('gulp-sass');
+const cleanCSS   = require('gulp-clean-css');
+const plumber    = require('gulp-plumber');
+const sequence   = require('gulp-sequence');
+const notify     = require('gulp-notify');
+const del        = require('del');
+const minify     = require("gulp-babel-minify");
+
+const src = 'src';
+const dist = 'public';
+
+function reportError(error) {
+  // Trigger Mac OS notification
+  notify({
+    title: `Task Failed [${error.plugin}]`,
+    message: 'Check the console.'
+  }).write(error);
+  // Log the error in the terminal
+  console.log(error.toString());
+  // Prevents having to restart Gulp after a crash
+  this.emit('end');
+}
+
+gulp.task('clean', () => {
+  return del([`${dist}/css`, `${dist}/js`, `${dist}/assets`]);
+});
+
+gulp.task('scripts', () => {
+  return gulp.src(`${src}/**/*.js`)
+    .pipe(plumber({ errorHandler: reportError }))
+    .pipe(babel({ presets: ['es2015']}))
+    .pipe(minify({ mangle: { keepClassName: true }}))
+    .pipe(gulp.dest(dist));
+});
+
+gulp.task('sass', () => {
+  return gulp.src(`${src}/**/style.scss`)
+    .pipe(plumber({ errorHandler: reportError }))
+    .pipe(sass())
+    .pipe(cleanCSS({ compatibility: 'ie8'}))
+    .pipe(gulp.dest(dist));
+});
+
+gulp.task('assets', () => {
+  return gulp.src(`${src}/assets/**/*`)
+  .pipe(gulp.dest(`${dist}/assets`));
+});
+
+gulp.task('watch', () => {
+  gulp.watch(`${src}/**/*.js`, ['scripts']);
+  gulp.watch(`${src}/**/*.scss`, ['sass']);
+  gulp.watch(`${src}/assets/**/*`, ['assets']);
+});
+
+gulp.task('default', sequence('clean', ['scripts', 'sass', 'assets'], 'watch'));
---
+++ readme.md
@@ -0,0 +1,9 @@
+Starter code for a basic HTML JS CSS site, with npm (for gulp):to transpile and minify javascript and SASS.
+
+`Git Clone`
+
+`~ npm i`
+
+`~ gulp`
+
+The root index.html file is set up to work with the public JS and CSS, the default gulp task will watch the src JS and SASS, there is no live-reloading.
